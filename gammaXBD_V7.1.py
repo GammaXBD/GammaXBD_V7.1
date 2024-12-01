55d0f3f8769d4d355cb3cac084bcca867c4e9a19
GammaXBD <ankita.kataky1@gmail.com>
2024-12-01 23:33:21 +0600

    Add files via upload

---
+++ gammaXBD_V7.1.py
@@ -0,0 +1,12 @@
+import animation_gamma
+import backtest_gamma
+import gamma_otc
+import generate_gamma
+import quotex_gamma
+
+
+quotex_gamma.main()
+backtest_gamma.main()
+gamma_otc.main()
+generate_gamma.main()
+animation_gamma.main()
